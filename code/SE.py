c89413ab469c156068a6536210bc6ba338cddfa6
unknown <17865197113@163.com>
2022-02-14 17:56:57 +0800

    add SE and CBAM module

---
+++ code/SE.py
@@ -0,0 +1,28 @@
+from torch import nn
+import torch
+import torch.nn.functional as F
+
+
+class SE(nn.Module):
+    def __init__(self, channels, reduction=2):
+        super(SE, self).__init__()
+        self.squeeze = nn.AdaptiveAvgPool2d((1, 1))
+        self.excitation = nn.Sequential(
+            nn.Conv2d(channels, channels // reduction, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channels // reduction, channels, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.Sigmoid())
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.squeeze(x)
+        print(y.shape)
+        y = self.excitation(y)
+        print(y.shape)
+        return x * y
+
+X = torch.rand((4,4,4,5))
+se = SE(4)
+Y = se(X)
+print(Y)
+
